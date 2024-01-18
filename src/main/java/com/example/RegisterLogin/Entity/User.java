8eb789c2d2df0d07febd4feb526d8d1b1cc84e61
Aliya <ajaliya287@gmail.com>
2024-01-18 12:48:22 +0530

    commit

--- src/main/java/com/example/RegisterLogin/Entity/User.java
+++ src/main/java/com/example/RegisterLogin/Entity/User.java
@@ -1,5 +1,4 @@
 package com.example.RegisterLogin.Entity;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
