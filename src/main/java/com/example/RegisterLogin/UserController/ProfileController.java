aa5f7f8d5007e32c2a91efe3528c42ec8b866c5b
mohammed althaf <mohammedalthaf1111@gmail.com>
2024-02-14 13:18:50 +0530

    profileservice added to fg-sub

--- src/main/java/com/example/RegisterLogin/UserController/ProfileController.java
+++ src/main/java/com/example/RegisterLogin/UserController/ProfileController.java
@@ -1,6 +1,5 @@
 package com.example.RegisterLogin.UserController;// ProfileController.java
 import com.example.RegisterLogin.Entity.User;
-import com.example.RegisterLogin.Service.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.RegisterLogin.Service.ProfileService;
 
 @RestController
 @RequestMapping("/api/profile")
